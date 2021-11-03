f99d6b3124873afbc9a9ddeb0070b3706f1c0107
williansimioni <simioniwb@gmail.com>
2021-11-03 17:45:16 -0300

    Alterações no script

--- 01_scripts/bandeira.R
+++ 01_scripts/bandeira.R
@@ -1,15 +1,29 @@
+library(readr)
+library(here)
 
-bandeira<-readr::read_csv('tamandua_bandeira.csv.csv')
+#Importando a planilha da spp T. bandeira
+bandeira <- readr::read_csv(here::here("00_dadosbrutos", 'tamandua_bandeira.csv.csv'))
 bandeira
 
+#Importando a planilha das duas especies
+xen_spp <- readr::read_csv(here::here("00_dadosbrutos", 'tamanduas.csv.csv'))
+xen_spp
 
 # nº de indivídiuos por habitat
-
+# Abundancia da spp. bandeira
 bandeira |>  
   ggplot2::ggplot(ggplot2::aes(y=habitat))+
   ggplot2::geom_bar()+
   ggplot2::theme_classic()+
-  ggplot2::xlab(label = 'Densidade')+
+  ggplot2::xlab(label = 'Abundância')+
+  ggplot2::ylab(label= 'Habitat')
+
+# Abundancia das duas spp.
+xen_spp |>  
+  ggplot2::ggplot(ggplot2::aes(y=habitat))+
+  ggplot2::geom_bar()+
+  ggplot2::theme_classic()+
+  ggplot2::xlab(label = 'Abundância')+
   ggplot2::ylab(label= 'Habitat')
 
 
