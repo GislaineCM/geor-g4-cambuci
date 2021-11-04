fdae11bfa00397e1cce45285a8d820f3f3b4465b
williansimioni <simioniwb@gmail.com>
2021-11-03 21:36:39 -0300

    Alterações no script

--- 01_scripts/bandeira.R
+++ 01_scripts/bandeira.R
@@ -1,12 +1,27 @@
+#' ---
+#' Titulo: Projeto Final - Analise de dados geoespaciais no R
+#' Autores: Daiana, Gislaine, Luana, Rafael, Willian
+#' Data: 2021-11-03
+#' ---
+
+# PERGUNTA: Entender como os tipos de habitats influenciam a abundância de 
+# tamandua-bandeira (Myrmecophaga tridactyla) e de tamandua-mirim (Tamandua 
+# tetradactyla) no Brasil.
+
+# OBJETIVO: 
+
+
+# Carregando os pacotes necessários
 library(readr)
 library(here)
+library(ggplot2)
 
-#Importando a planilha da spp T. bandeira
+#Importando os dados de ocorrencia das spp
 dados_tamandua <- readr::read_csv(here::here("00_dadosbrutos", 'tamanduas.csv'))
 dados_tamandua
 
 
-# nº de indivídiuos por habitat
+# n de indivídiuos por habitat
 # Abundancia das duas spp.
 dados_tamandua |>  
   ggplot2::ggplot(ggplot2::aes(y=habitat))+
@@ -16,8 +31,7 @@ dados_tamandua |>
   ggplot2::ylab(label= 'Habitat')
 
 
-#bandeira |>
+#dados_tamandua |>
   #ggplot2::ggplot(ggplot2::aes(x=as.numeric(long_x),y=as.numeric(lat_y)))+
   #ggplot2::geom_jitter(ggplot2::aes(col=as.character(year)))
 
-#dendograma fazeer 
